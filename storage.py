f40c8b2770e86e259c78534a3bc7f542f6aae1bd
egorm <egormishalkin@yandex.ru>
2026-07-16 04:08:13 +0300

    Improve habits loading

--- storage.py
+++ storage.py
@@ -1,16 +1,15 @@
 import json
 
 def load_habits():
-    # Открываем файл для чтения
     try:
         with open("habits.json", "r", encoding="utf-8") as f:
-            # Загружаем данные из файла в переменную
             habits_data = json.load(f)
             return habits_data
     except FileNotFoundError:
-        print("файл не найден =(")
+        return {}
+    except json.JSONDecodeError:
+        return {}
 
 def save_habits(habits):
-    # Writing JSON data to a file
     with open("habits.json", "w", encoding="utf-8") as file:
         json.dump(habits, file, ensure_ascii=False, indent=4)
